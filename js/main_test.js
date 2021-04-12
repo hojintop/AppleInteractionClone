5ea767d413c59d9e5a6693a73d70d01fcc8f73e3
hojintop <hojintop@gmail.com>
2021-04-12 22:00:29 +0900

    "scene 이 변경될때 opacity 증감소 값에 대해 비정상적으로 입력되는 경우를 대비하여 scene 이 변경될때를 감지하여 playAnimation 호출을 한번 pass 하고 실행할수 있도록 flag 값 추가하여 제어"

--- js/main_test.js
+++ js/main_test.js
@@ -3,7 +3,7 @@
 	let yOffset = 0;		//  window.pageYOffset 대입될 변수
 	let prevScrollHeight = 0;		//  현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
 	let currentScene = 0;		//  현재 활성화된(눈 앞에 보고 있는) 씬(scroll-section)
-
+	let enterNewScene = false;		//  새로운 scene 이 시작되는 순간 true
 
 	const sceneInfo = [
 		{
@@ -104,17 +104,20 @@
 	}
 
 	function scrollLoop(){
+		enterNewScene = false;
 		prevScrollHeight = 0;
 		for(let i=0;i<currentScene;i++){
 			prevScrollHeight += sceneInfo[i].scrollHeight;
 		}
 
 		if(yOffset>prevScrollHeight+sceneInfo[currentScene].scrollHeight){
+			enterNewScene = true;
 			currentScene++;
 			document.body.setAttribute('id',`show-scene-${currentScene}`);
 		}
 
 		if(yOffset<prevScrollHeight){
+			enterNewScene = true;
 			if(currentScene === 0){		//  브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지(모바일)
 				return;
 			}
@@ -125,6 +128,10 @@
 		// 새로고침 될때 어차피 id 값으로 속석값이 들어감으로 주석후 위 스크롤되어 현재 씬이 변경될때만 적용되도록 한다.
 		// document.body.setAttribute('id',`show-scene-${currentScene}`);
 
+		if (enterNewScene){
+			return;	
+		}
+		
 		playAnimation();
 	}
 
